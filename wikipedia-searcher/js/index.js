b14fdfcc4e272ca08909b1e228add54cda7992b4
Charlotte Joy Callender <charlottejoyliving@gmail.com>
2018-08-03 11:46:59 -0400

    Show excerpt, style tweeks

--- wikipedia-searcher/js/index.js
+++ wikipedia-searcher/js/index.js
@@ -14,36 +14,38 @@ $(document).ready(function () {
       $(".content").empty();
 
       function writeResults() {
-
-
         if (articles.hasOwnProperty("query") == false) {
           $(".content").append("<div id='no-results' class='container'><h2>No search results found, please try again</h2></div>");
           $(".content").slideDown();
         } else {
-          $("#welcome").hide();
+          $("#intros").hide();
           for (var pageID in articles.query.pages) {
             //this gets the page ids (the value of properties within pages)
-            // this is a nice to have alert(articles.query["pages"][x]["images"][0]["title"]);
+
+            var excerpt;
+            var maxChar;
+
+            window.innerHeight <= 500 || window.innerWidth <= 450 ? maxChar = 100 : maxChar = 250;
+
+            (articles.query["pages"][pageID]["extract"]).length >= 80 ?
+            excerpt= (articles.query["pages"][pageID]["extract"]).slice(0, maxChar) + "..." :  excerpt=articles.query["pages"][pageID]["extract"];
 
             $(".content").append("<div class='article-div'><div class='article-content'><p><b>" +
               articles.query["pages"][pageID]["title"] +
-              "</b></p><p>" +
-              articles.query["pages"][pageID]["extract"] +
+              "</b></p><p>" + excerpt +
               "</p><p class='readmore'><a href='" +
               articles.query["pages"][pageID]["fullurl"] +
               "' target='blank'>Click to learn more</a></p></div></div>");
 
             $(".article-div").mouseenter(function () {
               $(".readmore", this).css("text-decoration", "underline");
-              $(".readmore a", this).css("background-color", "#B3E5FC");
-              $(".readmore a", this).css("box-shadow", "1px 2px 2px rgba(0, 0, 0, .29)");
+              $(".readmore a", this).css({"background-color": "#B3E5FC","box-shadow":"1px 2px 2px rgba(0, 0, 0, .29)"});
   
             });
 
             $(".article-div").mouseleave(function () {
               $(".readmore", this).css("text-decoration", "none");
-              $(".readmore a", this).css("background-color", "white");
-              $(".readmore a", this).css("box-shadow", "none");
+              $(".readmore a", this).css({"background-color":"white","box-shadow": "none"});
             });
 
             $(".content").slideDown();
@@ -75,7 +77,7 @@ $(document).ready(function () {
 
 /*Nice to haves:
 Bonus User Story: as they type,they see a dropdown for autocomplete option with matching wikipedia entries
-Learn to access the image from each entry
+Learn to access the image from each entry articles.query["pages"][x]["images"][0]["title"];
 Let user choose how many results to see
 Nice to haves from API:
 Learn about action=raw to get wiki markup
